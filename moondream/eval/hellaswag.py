4556f38a9c3a0767d9f8d13ae5fa42840c8ed701
jamari-morrison <redcater360@gmail.com>
2025-02-25 02:09:02 +0000

    apply formatting

--- moondream/eval/hellaswag.py
+++ moondream/eval/hellaswag.py
@@ -13,39 +13,34 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 def parse_answer(input_text):
     # Find all numbers in the text
-    numbers = re.findall(r"\d+", input_text)
-
+    numbers = re.findall(r'\d+', input_text)
+    
     # Return the last number if found, otherwise None
     return numbers[-1] if numbers else None
 
-
-def eval_mmlu(model, debug=False):
-    """Evaluate the model on the MMLU dataset."""
+def eval_hellaswag(model, debug=False):
+    """Evaluate the model on the hellaswag dataset."""
     # Load the hellaswag dataset, validation is 10k rows so only grab 1k random ones for our eval here
-    mmlu_test = (
-        load_dataset("Rowan/hellaswag", split="validation")
-        .shuffle(seed=42)
-        .select(range(1000))
-    )
+    hellaswag_test = load_dataset("Rowan/hellaswag", split="validation").shuffle(seed=42).select(range(1000))
 
     correct = 0
     total = 0
     results = []
 
-    for row in tqdm(mmlu_test, disable=debug, desc="HellaSwag"):
+    for row in tqdm(hellaswag_test, disable=debug, desc="HellaSwag"):
         question = (
-            "Return the number of the best ending for this passage.\n\n"
+            'Return the number of the best ending for this passage.\n\n'
             f'Passage: "{row["activity_label"]}. {row["ctx"]}"\n\n'
-            "Endings:\n"
+            'Endings:\n'
             f'0. {row["endings"][0]}\n'
             f'1. {row["endings"][1]}\n'
             f'2. {row["endings"][2]}\n'
             f'3. {row["endings"][3]}\n'
-            f"Your response should be a single number representing the correct ending- 0, 1, 2, or 3."
+            f'Your response should be a single number representing the correct ending- 0, 1, 2, or 3.'
         )
 
         # Extract the ground truth answer from the answer field, just the number
-        gt_answer = int(row["label"])
+        gt_answer = int(row['label'])
 
         # Encode the question for the model
         model_response = model._text_query(question)["answer"]
@@ -55,9 +50,9 @@ def eval_mmlu(model, debug=False):
         # Convert to float for comparison (handling both integers and decimals)
         try:
             if int(model_answer) == int(gt_answer):
-                is_correct = True
+                is_correct=True
             else:
-                is_correct = False
+                is_correct=False
         except:
             is_correct = False
 
@@ -112,6 +107,6 @@ if __name__ == "__main__":
     # Compile omitted to make text only work
     # model.compile()
 
-    result = eval_mmlu(model, args.debug)
+    result = eval_hellaswag(model, args.debug)
 
     print(f"Accuracy: {result['accuracy']:.4f} ({result['correct']}/{result['total']})")
